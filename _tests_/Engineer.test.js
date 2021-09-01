a10468f2cc9499e5ddcddf91246824f7ba626f27
Tina Pham <tina.pham8912@gmail.com>
2021-08-31 19:28:24 -0700

    passed test for engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -7,9 +7,33 @@ test("it should create an object when I initialize my Engineer class", () => {
     expect(typeof(testVariable)).toBe("object")
 })
 
-test("if I pass a first parameter it should assign a github username in the object", () => {
-    let testVariable = new GitHub("tpham8")
+test("if I pass a first parameter it should assign a name in the object", () => {
+    let testVariable = new Engineer("Mike")
 
     console.log(testVariable)
-    expect(testVariable.GitHub).toBe("tpham8")
+    expect(testVariable.name).toBe("Mike")
+})
+
+test("if I pass a second parameter it should assign an id in the object", () => {
+    let testVariable = new Engineer("Mike","007")
+    
+    console.log(testVariable)
+    expect(testVariable.id).toBe("007")
+
+})
+
+test("if I pass a third parameter it should assign an email in the object", () => {
+    let testVariable = new Engineer("Mike","007","mike@yahoo.com")
+    
+    console.log(testVariable)
+    expect(testVariable.email).toBe("mike@yahoo.com")
+
+})
+
+test("if I pass a fourth parameter it should assign a github username in the object", () => {
+    let testVariable = new Engineer("Mike","007","mike@yahoo.com","mike88")
+    
+    console.log(testVariable)
+    expect(testVariable.github).toBe("mike88")
+
 })
